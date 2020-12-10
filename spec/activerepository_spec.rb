e7b4674719d6135bbf6418017726a08b36bd79e2
Mohamed Camara GUEYE <katakeynii@gmail.com>
2020-12-10 15:03:32 +0000

    repository aggregate module file

--- spec/activerepository_spec.rb
+++ spec/activerepository_spec.rb
@@ -1,9 +1,9 @@
-RSpec.describe Activerepository do
+RSpec.describe ActiveRepository do
   it "has a version number" do
-    expect(Activerepository::VERSION).not_to be nil
+    expect(ActiveRepository::VERSION).not_to be nil
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "activerepository"
+require "active_repository"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
