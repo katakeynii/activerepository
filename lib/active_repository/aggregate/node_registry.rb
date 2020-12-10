195c130bbb3d7d0650b219f33e95349b33258a5c
Mohamed Camara GUEYE <katakeynii@gmail.com>
2020-12-10 14:55:13 +0000

    registry classes

---
+++ lib/active_repository/aggregate/node_registry.rb
@@ -0,0 +1,35 @@
+module ActiveRepository
+
+    module Aggregate
+      class NodeRegistry
+        attr_reader :registrations
+
+        def initialize
+            @registrations = []
+        end
+    
+        def register(name, target_klass)
+            node = Item.new(name: name , klass: target_klass )
+            registrations << node
+        end
+
+        def find(node_name)
+            registration = registration.find {|r| r.name === node_name}
+            raise ArgumentError, "We dont know this node !" if registration.nil?
+        end
+
+        private
+      end
+
+      class Item
+        attr_reader :name, :klass 
+        def initialize(name, klass)
+          @name = name
+          @klass = Object.const_get(klass)
+        end
+        def method_missing(method_name, *args, &block)
+          @klass.send(method_name, *args, &block)
+        end
+      end
+    end
+end
