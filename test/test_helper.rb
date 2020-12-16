f1703ad595259fc49c18f5a3b65096b141ee3c9c
Mohamed Camara GUEYE <katakeynii@gmail.com>
2020-12-16 16:34:31 +0000

    updating test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,9 +8,22 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
+require "rails_app/config/environment"
+require "rails/test_help"
+
+
+module TestSetup
+    extend ActiveSupport::Concern
+  
+    included do
+      # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+      fixtures :all
+    end
+end
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+    include TestSetup
+end
 
-
+class Minitest::Test
+    include TestSetup
 end
