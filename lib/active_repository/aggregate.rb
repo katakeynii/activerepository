61f973b6fabb77dd4eee96656bef142cab73f09b
Mohamed Camara GUEYE <katakeynii@gmail.com>
2020-12-18 17:21:50 +0000

    udpating node repository calling

--- lib/active_repository/aggregate.rb
+++ lib/active_repository/aggregate.rb
@@ -5,18 +5,23 @@ module ActiveRepository
     module Aggregate
 
         extend ActiveSupport::Concern
-        @@nodes = Aggregate::Nodes.new
-
+        autoload :Nodes, 'active_repository/aggregate/nodes'
+        autoload :NodeRegistry, 'active_repository/aggregate/node_registry'
+        included do 
+        end
         module ClassMethods
+            @@nodes = Aggregate::Nodes.new
 
             def nodes
-                # @@nodes.registry
+                repository =  self.name.underscore.to_sym
+                @@nodes.set_repository(repository).registry
             end
             def aggregate &block
                 block.call if block_given?
             end
             def node name, target
-                # @@nodes.register(name, target)
+                repository =  self.name.underscore.to_sym
+                @@nodes.register(name, target, repository)
             end
         end
 
