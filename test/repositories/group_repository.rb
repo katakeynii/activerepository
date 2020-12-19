91cf6daa0cca6cb9dc8cc455bacac0fb80dd0e2e
Mohamed Camara GUEYE <katakeynii@gmail.com>
2020-12-19 01:45:49 +0000

    updating repository for test

--- test/repositories/group_repository.rb
+++ test/repositories/group_repository.rb
@@ -1,8 +1,6 @@
 require "models/blog/group"
 require "active_repository"
 class GroupRepository < ActiveRepository::Base
-    aggregate do 
-        node :group, "Blog::Group"
-    end
+    model :group, "Blog::Group"
     
 end
