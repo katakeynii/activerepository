2d8a76b0226a6419b87e3ea717207fa24bee8673
Mohamed Camara GUEYE <katakeynii@gmail.com>
2020-12-20 03:20:19 +0000

    adding aggregate options

--- lib/generators/repository/repository_generator.rb
+++ lib/generators/repository/repository_generator.rb
@@ -6,18 +6,19 @@ require "rails/generators/active_record"
 module Rails
   module Generators
     class RepositoryGenerator < Rails::Generators::NamedBase
-      include Rails::Generators::ResourceHelpers
+
       argument :attributes, type: :array, default: [], banner: "name[:type] name[:type]"
+      class_option :aggregate, type: :boolean, default: false
       source_root File.expand_path('templates', __dir__)
       # creates the migration file for the model.
 
       def create_repository_file
         generate_abstract_class 
         @module_name = options[:module]
-
+        @aggregate = options['aggregate']
         service_dir_path = "app/repositories"
         generator_dir_path = service_dir_path + ("/#{@module_name.underscore}" if @module_name.present?).to_s
-            generator_path = generator_dir_path + "/#{file_name}_repository.rb"
+        generator_path = generator_dir_path + "/#{file_name}_repository.rb"
 
         Dir.mkdir(service_dir_path) unless File.exist?(service_dir_path)
         Dir.mkdir(generator_dir_path) unless File.exist?(generator_dir_path)
