f01680a0d55973ceed2fd7f312993378e986f5ec
Mohamed Camara GUEYE <katakeynii@gmail.com>
2020-12-24 22:41:18 +0000

    adding error modules

--- lib/active_repository.rb
+++ lib/active_repository.rb
@@ -10,12 +10,17 @@ module ActiveRepository
   autoload :Aggregate, "active_repository/aggregate"
   autoload :FnCaller, "active_repository/fn_caller"
   autoload :Base ,     "active_repository/base"
+  autoload :Error ,     "active_repository/error"
+
+
 
   module Aggregate
     autoload :Nodes,           'active_repository/aggregate/nodes'
     autoload :NodeRegistry,    'active_repository/aggregate/node_registry'
   end
 
+  module QueryBuilder
+  end
   module FnCaller 
     autoload :PostgresFn, 'active_repository/fn_caller/postgres_fn'
   end
