2e45676421aebc2e8d05a733aa1d5e387d860970
Mohamed Camara GUEYE <katakeynii@gmail.com>
2020-12-26 10:52:11 +0000

    Fixing issues #4

--- lib/active_repository/aggregate/node_registry.rb
+++ lib/active_repository/aggregate/node_registry.rb
@@ -18,8 +18,10 @@ module ActiveRepository
             if root.eql?(true)
               root_item = find_root repository
 
-              # raise MultipleRootError if !root_item.nil? && !root_item.name.eql?(name)
-              # raise MultipleRootError if !root_item.nil? 
+              unless root_item.nil?
+                raise MultipleRootError if !root_item.name.eql?(name)
+              end
+
             end
             _node = find(name, repository)
             registrations[repository] << node if _node.nil?
