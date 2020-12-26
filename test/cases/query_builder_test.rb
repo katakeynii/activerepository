6f1246ec76ebeda71bf943bb6855d62fbbc6976f
Mohamed Camara GUEYE <katakeynii@gmail.com>
2020-12-26 13:36:51 +0000

    commenting test line in qb

--- test/cases/query_builder_test.rb
+++ test/cases/query_builder_test.rb
@@ -10,12 +10,7 @@ class AggregateTest < ActiveSupport::TestCase
             qb = ActiveRepository::QueryBuilder::Query.new
             qb2 = ActiveRepository::QueryBuilder::Query.new
 
-            puts qb2.fn("generate_series", "date '2020-11-01 00:00'", "date '2020-11-30 00:00'",  "'1 day'::interval").get_result
-            # dd = qb.from("articles", "a")
-        #     puts "=" * 50
-        #     puts qb.from("climat_quotidiens", "cq")
-        #     .select(qb.cq[:temp_moy_c], qb.cq[:frame_date])
-        #     .get_result
+            # puts qb2.fn("generate_series", "date '2020-11-01 00:00'", "date '2020-11-30 00:00'",  "'1 day'::interval").get_result
         end
     end
 
