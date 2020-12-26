e84b3adf2f178c19e893254c42ba3689b7656819
Mohamed Camara GUEYE <katakeynii@gmail.com>
2020-12-26 13:38:56 +0000

    commenting test line in qb

--- test/cases/query_builder_test.rb
+++ test/cases/query_builder_test.rb
@@ -7,10 +7,6 @@ class AggregateTest < ActiveSupport::TestCase
 
     describe ActiveRepository::QueryBuilder do
         it "must contain at least node" do
-            qb = ActiveRepository::QueryBuilder::Query.new
-            qb2 = ActiveRepository::QueryBuilder::Query.new
-
-            # puts qb2.fn("generate_series", "date '2020-11-01 00:00'", "date '2020-11-30 00:00'",  "'1 day'::interval").get_result
         end
     end
 
